daa6908a30b9a6dd9832cf94381a76c8cd40ed16
that guy <myself@gargleyark.com>
2020-10-25 17:38:21 +0000

    commits fe build files

---
+++ server/public/material-dashboard-react/precache-manifest.d33af2c6fbfe40b06b1b070ee6fd803d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60de17f7cea5a8291bd2616802920071",
+    "url": "/material-dashboard-react/index.html"
+  },
+  {
+    "revision": "fcc3eb0fa9f15bb11da3",
+    "url": "/material-dashboard-react/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "f9482736ebcaca756283",
+    "url": "/material-dashboard-react/static/css/main.0fa7ec49.chunk.css"
+  },
+  {
+    "revision": "fcc3eb0fa9f15bb11da3",
+    "url": "/material-dashboard-react/static/js/2.eb79d190.chunk.js"
+  },
+  {
+    "revision": "f9482736ebcaca756283",
+    "url": "/material-dashboard-react/static/js/main.91ecaf71.chunk.js"
+  },
+  {
+    "revision": "64cae1c158f409cf12ba",
+    "url": "/material-dashboard-react/static/js/runtime~main.ebe0c747.js"
+  },
+  {
+    "revision": "a9af37d8df946f2193e397853004b2e7",
+    "url": "/material-dashboard-react/static/media/logo_small.a9af37d8.png"
+  },
+  {
+    "revision": "3e095a60b9d874803a5e2f30fe4ecb5e",
+    "url": "/material-dashboard-react/static/media/marc.3e095a60.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/material-dashboard-react/static/media/sidebar-2.310509c9.jpg"
+  }
+]);
